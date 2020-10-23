a97aa6ec21721942eeeb8e0abc6aedd897238c8b
RUCHIT680 <ruchitrai282@gmail.com>
2020-10-24 02:27:50 +0530

    added

--- src/hashtable/MyHashMap.java
+++ src/hashtable/MyHashMap.java
@@ -1,33 +1,50 @@
 package hashtable;
 
-public class MyHashMap {
-	MyLinkedList<K> myLinkedList;
+import java.util.ArrayList;
 
-	public MyHashMap() {
-		this.myLinkedList = new MyLinkedList<>();
+public class MyLinkedHashMap<K,V>{
+	private final int numBuckets;
+	ArrayList<MyLinkedList<K>> myBucketArray;
+
+	public MyLinkedHashMap() {
+		this.numBuckets=10;
+		this.myBucketArray=new ArrayList<>(numBuckets);
+
+		for (int i=0;i<numBuckets;i++)
+			this.myBucketArray.add(null);
 	}
 
-	public V get(K key) {
-		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkedList.search(key);
-		return (myMapNode == null) ? null : myMapNode.getValue();
+	public V getMap(K key) {
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList=this.myBucketArray.get(index);
+		if(myLinkedList==null)return null;
+		MyMapNode<K,V> myMapNode=(MyMapNode<K,V>)myLinkedList.search(key);
+		return (myMapNode==null)? null:myMapNode.getValue();
 	}
 
-	public void add(K key, V value) {
-		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkedList.search(key);
-		if (myMapNode == null) {
-			myMapNode = new MyMapNode<>(key, value);
-			this.myLinkedList.append(myMapNode);
-		} else {
+	private int getBucketIndex(K key) {
+		int hashCode=Math.abs(key.hashCode());
+		int index=hashCode%numBuckets;
+		System.out.println("Key "+key+" hashcode: "+hashCode+" index: "+index);
+		return index;
+	}
+
+	public void addMap(K key, V value) {
+		int index=this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList=this.myBucketArray.get(index);
+		if(myLinkedList==null) {
+			myLinkedList=new MyLinkedList<>();
+			this.myBucketArray.set(index, myLinkedList);
+		}
+		MyMapNode<K,V> myMapNode=(MyMapNode<K,V>) myLinkedList.search(key);
+		if(myMapNode==null) {
+			myMapNode=new MyMapNode<>(key,value);
+			myLinkedList.append(myMapNode);
+		}else {
 			myMapNode.setValue(value);
 		}
-
 	}
-
 	@Override
-	public String toString() {
-		return "MyHashMapNodes{" + myLinkedList + '}';
-	}
-
+	public String toString() { return "MyLinkedHashMap List{" +myBucketArray+'}';}
 }
-
 
